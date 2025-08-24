0ced1aa2b33a7ab816249a62b103b11e1ccee28f
lailasam <samaralailaa@gmail.com>
2025-08-24 19:45:40 -0300

    finalizando classe spaces

--- br.com.sudoku/sudoku-dio/src/model/Spaces.java
+++ br.com.sudoku/sudoku-dio/src/model/Spaces.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Spaces {
-
+private boolean isFixed;
+private Integer number; //dessa forma pode ser null
+public Spaces(boolean isFixed, Integer number) {
+    this.isFixed = isFixed;
+    this.number = number;
+}
+public boolean isFixed() {
+    return isFixed;
+}
+public void setFixed(boolean isFixed) {
+    this.isFixed = isFixed;
+}
+public Integer getNumber() {
+    return number;
+}
+public void setNumber(Integer number) {
+    this.number = number;
+}
 }
